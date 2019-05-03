88a0901216fd1492f33822c91b416cfb0470a666
cosmo4 <warner.luke0102@gmail.com>
2019-05-03 13:07:36 -0600

    Need grade

---
+++ ChatServer/src/com/muc/Server.java
@@ -0,0 +1,41 @@
+package com.muc;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Server extends Thread{
+    private final int serverPort;
+
+    private ArrayList<ServerWorker> workerList = new ArrayList<>();
+
+    public Server(int serverPort) {
+        this.serverPort = serverPort;
+    }
+
+    public List<ServerWorker> getWorkerLIst() {
+        return workerList;
+
+    }
+
+    @Override
+    public void run() {
+        try {
+            ServerSocket serverSocket = new ServerSocket(serverPort);
+            while (true) {
+                System.out.println("About to accept client connection...");
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("AcceptedConnection from " + clientSocket);
+                ServerWorker worker = new ServerWorker(this, clientSocket);
+                worker.start();
+            }
+
+        }catch (IOException e) {
+            e.printStackTrace();
+
+        }
+    }
+
+}
